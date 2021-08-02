27c28c339b3f48c301464de8088c166f6af9e5f2
doorgesh neetye <neetyedoorgesh@gmail.com>
2021-08-02 18:00:37 +0400

    cruise validation done and user phonenumber validation corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :cruises
 
   validates :username, uniqueness: true, presence: true
+  validates :phone_number, uniqueness: true, presence: true, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed."}
+
   validates :address, presence: true
   validates :email, format: { with: /\A.*@.*\.com\z/ }
   validates :password, length: { minimum: 6 }
