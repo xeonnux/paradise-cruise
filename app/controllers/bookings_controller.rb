944919a94b50162587cc7a355de2d4e28206c769
Dilan Sookun <dilansookun@gmail.com>
2021-08-02 18:01:00 +0400

    defined methods in controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,35 @@
 class BookingsController < ApplicationController
+  def new
+    @cruise = Cruise.find(params[:cruise_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @cruise = Cruise.find(params[:cruise_id])
+    @booking = Booking.new(booking_params)
+    @booking.cruise = @cruise
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      @bookings = @cruise.bookings
+      render 'cruises/show'
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @cruise = Cruise.find(@booking.cruise_id)
+    if Booking.delete(params[:id]).positive? 
+      flash[:success] = 'Booking has been cancelled'
+    else
+      flash[:error] = 'Sorry something went wrong, please try again'
+    end
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:total_price, :schedule_date, :available_seats)
+  end
 end
--- app/controllers/cruises_controller.rb
+++ app/controllers/cruises_controller.rb
@@ -1,2 +1,33 @@
 class CruisesController < ApplicationController
+  def new
+    @cruise = Cruise.new
+  end
+
+  def create
+    @cruise = Cruise.new(cruise_params)
+    @cruise.save
+    redirect_to cruise_path(@cruise)
+  end
+
+  def update
+    @cruise = Cruise.find(params[:id])
+    @cruise.update(cruise_params)
+    flash[:notice] = 'Cruise information has been updated'
+    redirect_to cruise_path
+  end
+
+  def destroy
+    @cruise.destroy
+    redirect_to cruises_path
+  end
+
+  private
+
+  def set_cruise
+    @cruise = Cruise.find(params[:id])
+  end
+
+  def cruise_params
+    params.require(:cruise).permit(:name, :description, :duration_of_cruise, :capacity, :extra_offers, :cancellation_policy, :location, :price)
+  end
 end
