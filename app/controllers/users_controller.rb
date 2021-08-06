62bfe9506760f29432f190f9653623279fe0ef15
Fabrice Madre <stepwiseltd@gmail.com>
2021-08-06 12:26:04 +0400

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
+    @bookings = Booking.all
+    @fidelity_point = 0
+    @bookings.each do |booking|
+      @fidelity_point += (booking.total_price / 100) * 10 if booking.user == current_user
+    end
   end
-
 end
