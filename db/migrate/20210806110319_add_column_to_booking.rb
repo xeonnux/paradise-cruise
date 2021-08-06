8b76dccebdb0c3f6e8d6b547be88a213818ad27e
Fabrice Madre <stepwiseltd@gmail.com>
2021-08-06 15:11:59 +0400

    change table column format

--- db/migrate/20210806110319_add_column_to_booking.rb
+++ db/migrate/20210806110319_add_column_to_booking.rb
@@ -1,6 +1,7 @@
 class AddColumnToBooking < ActiveRecord::Migration[6.0]
   def change
     remove_column :bookings, :schedule_date
+    remove_column :cruises, :duration_of_cruise
     add_column :cruises, :duration_of_cruise, :string
     add_column :bookings, :schedule_date, :string
   end
